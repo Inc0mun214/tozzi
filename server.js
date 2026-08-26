98196e087e0711a70c1fc8797e1cb0343ae9ed3a
Inc0mun214 <victmont13@gmail.com>
2026-08-26 15:42:31 -0300

    server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.delete('/api/produtos/:id', async (req, res) => {
 });
 
 // Fallback para SPA / Roteamento estático
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
